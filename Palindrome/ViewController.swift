16861c87de40e8b00912f4b91d90c6177304c6ff
Melissa Webster <mwebdesign@icloud.com>
2015-09-23 19:37:18 -0400

    Working Palindrome App

--- Palindrome/ViewController.swift
+++ Palindrome/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var wordToCompare = ""
+    var reverseWord = ""
 
+    @IBOutlet weak var palindromeResultLabel: UILabel!
+    @IBOutlet weak var userInputTextField: UITextField!
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func storeWordAsPalindrome() {
+        wordToCompare = userInputTextField.text!
+        reverseWord = ""
+        for scalar in wordToCompare.unicodeScalars {
+            let asString = "\(scalar)"
+            reverseWord = asString + reverseWord
+        }
+        
+    }
+  
+    @IBAction func checkWordButton() {
+        
+        storeWordAsPalindrome()
+        
+        
+        if wordToCompare == reverseWord {
+            palindromeResultLabel.text = ("\(wordToCompare) is a palindrome!")
+        } else {
+            palindromeResultLabel.text = ("\(wordToCompare) is NOT a palindrome :(")
+        }
+        
+    }
+   
 
 }
 
